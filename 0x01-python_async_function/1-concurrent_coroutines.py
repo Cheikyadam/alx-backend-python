02ac77fc76f23c13067bb1c69d464b96e61ec1e3
Cheikyadam <urbaingande@gmail.com>
2024-07-11 01:21:20 +0100

    1

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""multiple coroutines"""
+import asyncio
+import random
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int):
+    """wait random"""
+
+    if n <= 0:
+        return []
+
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    delays = []
+
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        delays.append(delay)
+
+    for i in range(len(delays) - 1, 0, -1):
+        if delays[i] < delays[i - 1]:
+            delays[i], delays[i - 1] = delays[i - 1], delays[i]
+        else:
+            break
+
+    return delays
