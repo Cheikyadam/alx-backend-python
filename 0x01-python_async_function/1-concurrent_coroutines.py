98e61978069a81d56032504b10124c8e936e7f57
Cheikyadam <urbaingande@gmail.com>
2024-07-11 01:22:50 +0100

    1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -5,7 +5,7 @@ import random
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int):
+async def wait_n(n: int, max_delay: int) -> list:
     """wait random"""
 
     if n <= 0:
