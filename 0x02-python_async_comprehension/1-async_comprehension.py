2d65eb8fdb3b36457e1aa11cbbdf86a98e7780c8
Cheikyadam <urbaingande@gmail.com>
2024-07-12 21:23:36 +0100

    1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -5,7 +5,4 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension():
     """the function def"""
-    result = []
-    async for i in async_generator():
-        result.append(i)
-    return result
+    return [number async for number in async_generator()]
